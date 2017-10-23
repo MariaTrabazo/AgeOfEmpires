1828e45d4d38c3d538f3069d154371be8ee68977
MariaTrabazo <32634321+MariaTrabazo@users.noreply.github.com>
2017-10-23 11:30:37 +0200

    Update Edificio.java

--- Edificio.java
+++ Edificio.java
@@ -11,7 +11,83 @@ package ageofempires_parte1;
  */
 public class Edificio {
     
-    private String ciudadela;
+     private String ciudadela;
+    private String casa;
+    private String cuartel;
+    private int puntosSalud;
+    private int costeReparacion;
+    private Recursos recurso;
+    
+    public Edificio(){
+        ciudadela = "Ciudadela";
+        casa = "Casa";
+        cuartel = "Cuartel";
+        puntosSalud=100;
+    }
+    
+    public Edificio(String valorCiudadela, String valorCasa, String valorCuartel, int puntosSalud, int costeReparacion){
+        ciudadela = valorCiudadela;
+        casa = valorCasa;
+        cuartel = valorCuartel;
+        this.puntosSalud=puntosSalud;
+        this.costeReparacion=costeReparacion;
+    }
+
+    public String getCiudadela() {
+        return ciudadela;
+    }
+
+    public void setCiudadela(String valorCiudadela) {
+        ciudadela = valorCiudadela;
+    }
+
+    public String getCasa() {
+        return casa;
+    }
+
+    public void setCasa(String valorCasa) {
+        casa = valorCasa;
+    }
+
+    public String getCuartel() {
+        return cuartel;
+    }
+
+    public void setCuartel(String valorCuartel) {
+        cuartel = valorCuartel;
+    }
+
+    public int getPuntosSalud() {
+        return puntosSalud;
+    }
+
+    public int getCosteReparacion() {
+        return costeReparacion;
+    }
+
+    public Recursos getRecurso() {
+        return recurso;
+    }
+
+    public void setPuntosSalud(int puntosSalud) {
+        if(puntosSalud==0){
+            System.out.println("No se puede reconstruir el edificio");
+        }
+        else{
+            this.puntosSalud = puntosSalud;
+        }
+        
+    }
+
+    public void setCosteReparacion(int costeReparacion) {
+        this.costeReparacion = costeReparacion;
+    }
+
+    public void setRecurso(Recursos recurso) {
+        this.recurso = recurso;
+    }
+    
+   /* private String ciudadela;
     private String casa;
     private String cuartel;
     
@@ -50,7 +126,7 @@ public class Edificio {
 
     public void setCuartel(String valorCuartel) {
         cuartel = valorCuartel;
-    }
+    }*/
     
     
 }
