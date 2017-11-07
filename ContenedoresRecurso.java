85a878776f1de6fb39c0f6ac0905411f90e1057c
MariaTrabazo <32634321+MariaTrabazo@users.noreply.github.com>
2017-11-08 00:24:51 +0100

    Update ContenedoresRecurso.java

--- ContenedoresRecurso.java
+++ ContenedoresRecurso.java
@@ -12,50 +12,34 @@ package ageofempires_parte1;
 public class ContenedoresRecurso {
     
     private String tipo;
+    private String nombre;
     private Recursos recurso;
     private Posicion posicion;
     private int cantidadMadera;
     private int cantidadPiedra;
     private int cantidadComida;
-    
-    public ContenedoresRecurso(){
-        
-        if("cantera".equals(tipo)){
-            cantidadPiedra = 100;
-            recurso.setPiedra(cantidadPiedra);
-        }
-        if("bosque".equals(tipo)){
-            cantidadMadera = 100;
-            recurso.setMadera(cantidadMadera);
-        }
-        if("arbusto".equals(tipo)){
-            cantidadComida = 100;
-            recurso.setComida(cantidadComida);
-        }
-        posicion = new Posicion();
-        
-    }
 
-    public ContenedoresRecurso(String tipo, Posicion posicion, int cantidadMadera, int cantidadPiedra, int cantidadComida) {
+    public ContenedoresRecurso(String tipo, Recursos recurso, Posicion posicion, int cantidadMadera, int cantidadPiedra, int cantidadComida) {
         this.tipo = tipo;
-        if("cantera".equals(tipo)){
-            this.cantidadPiedra = cantidadPiedra;
-            recurso.setPiedra(cantidadPiedra);
-         }   
-        if("bosque".equals(tipo)){
-            this.cantidadMadera = cantidadMadera;
-            recurso.setMadera(cantidadMadera);
-        }
-        if("arbusto".equals(tipo)){
-             this.cantidadComida = cantidadComida;
-             recurso.setComida(cantidadComida);
-        }
+        this.recurso = recurso;
         this.posicion = posicion;
-        
+        this.cantidadMadera = cantidadMadera;
+        this.cantidadPiedra = cantidadPiedra;
+        this.cantidadComida = cantidadComida;
     }
 
+    public ContenedoresRecurso() {
+        this.recurso = new Recursos();
+        this.posicion = new Posicion();
+        
+    }
+    
     public String getTipo() {
         return tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
     }
 
     public Recursos getRecurso() {
@@ -79,12 +63,17 @@ public class ContenedoresRecurso {
     }
 
     public void setTipo(String tipo) {
-        if(!"cantera".equals(tipo)|| !"arbusto".equals(tipo) ||!"bosque".equals(tipo)){
-            System.out.println("No hay contenedores de recurso de este tipo");
+        if("cantera".equals(tipo)|| "arbusto".equals(tipo) ||"bosque".equals(tipo)){
+            this.tipo = tipo; 
+            
         }
         else{
-           this.tipo = tipo; 
+           System.out.println("No hay contenedores de recurso de este tipo");
         }
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
     //FALTAN LAS COMPROBACIONES DE ESTOS SETTERS
     public void setRecurso(Recursos recurso) {
